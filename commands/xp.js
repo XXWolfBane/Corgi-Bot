9f4cd5fce4004331b339c3dfc1bedc4174d009e2
XXWolfBane <XXWolfBane@Gmail.com>
2019-05-02 14:17:52 -0400

    Create xp.js

---
+++ commands/xp.js
@@ -0,0 +1,20 @@
+module.exports.run = (bot, message, args, Discord, con) => {
+
+ con.query(`SELECT * FROM corgi_bot WHERE id = '${message.author.id}'`, (err, rows) => {
+  if(err) throw err;
+  
+  let xp = rows[0].xp
+  
+  let xpe = new Discord.RichEmbed()
+  .setTitle("User XP")
+  .setDescription(`Your XP is: ${xp}`)
+  .setColor("BLUE")
+  
+  
+  message.channel.send({embed: xpe})
+ })
+}
+
+module.exports.help = {
+  name: "xp"
+}
