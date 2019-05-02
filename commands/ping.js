7e04d616491136cab09d504593204492f300915a
XXWolfBane <XXWolfBane@Gmail.com>
2019-05-02 14:23:30 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,10 @@
 module.exports.run = (bot, message, args, Discord, con) => {
-console.log("Not broken")
+  let emb = new Discord.RichEmbed
+  .setTitle("Corgi Bot's Ping")
+  .setDescription("Heres my ping with Discord!")
+  .addField("Ping:", Math.floor(bot.ping))
+  
+  message.channel.send({embed: emb})
 }
 
 module.exports.help = {
