5cc70cf3e0edb292cf5982885424d7ab79b63e2a
XXWolfBane <XXWolfBane@Gmail.com>
2019-05-04 18:05:38 -0400

    Update eightball.js

--- commands/eightball.js
+++ commands/eightball.js
@@ -1,5 +1,5 @@
 module.exports.run = (bot, message, args, Discord, con) => {
-  let responces = ["blank", "Yes.", "No.", "Most likley", "Ask me agian"]
+  let responces = ["Yes.", "No.", "Most likley", "Ask me agian"]
   let responcea = Math.floor(Math.random()*4)
   message.channel.send(responces[responcea])
 }
