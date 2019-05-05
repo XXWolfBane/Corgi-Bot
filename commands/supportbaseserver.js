fcdb009b8b8e974e256d78eb237a9109e2db72a8
XXWolfBane <XXWolfBane@Gmail.com>
2019-05-05 03:54:52 -0400

    Update supportbaseserver.js

--- commands/supportbaseserver.js
+++ commands/supportbaseserver.js
@@ -2,7 +2,7 @@ module.exports.run = (bot, message, args, Discord, con) => {
   let em = new Discord.RichEmbed()
   .setTitle("Our Discord!")
   .setDescription("You can join this server for support!")
-  .addField("Click", `[here](https://discord.gg/QSzVUnZ) to join!`, inline=true)
+  .addField("Click", `[here](https://discord.gg/QSzVUnZ) to join!`, inline=false)
   .setColor("GREEN")
   
   message.channel.send({embed:em})
