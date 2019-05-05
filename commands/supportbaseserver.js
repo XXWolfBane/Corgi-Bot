6e052ce5f2ec4b656cff9609e7a60d7541d6beda
XXWolfBane <XXWolfBane@Gmail.com>
2019-05-05 03:32:05 -0400

    Create supportbaseserver.js

---
+++ commands/supportbaseserver.js
@@ -0,0 +1,12 @@
+module.exports.run = (bot, message, args, Discord, con) => {
+  let em = new Discord.RichEmbed()
+  .setTitle("Our Discord!")
+  .setDescription("You can join this server for support!")
+  .addField("Click", `[here](https://discord.gg/QSzVUnZ) to join!`)
+  
+  message.channel.send({embed:em})
+}
+
+module.exports.help = {
+  name: "corgibase"
+}
