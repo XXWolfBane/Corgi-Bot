f419b4336a76e6bc1a10296e738cec1d08c03dea
XXWolfBane <XXWolfBane@Gmail.com>
2019-05-06 18:26:14 -0400

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -2,11 +2,8 @@ module.exports.run = (bot, message, args, Discord, con) => {
   let responces = ["You work the in the corner office and you earn:", ""]
   let ae = Math.floor(Math.random()*5000)
   let ra = Math.floor(Math.random()*1)
-  let em = new Discord.RichEmbed
-  .setTitle("Corgi Work")
-  .addField(responces[ra], ae)
-  
-  message.channel.send({embed: em})
+
+  message.channel.send(`${responces[ra]}`)
   
   con.query(`UPDATE corgi_bot SET money = ${money + ae} WHERE id = '${message.author.id}'`)
 }
